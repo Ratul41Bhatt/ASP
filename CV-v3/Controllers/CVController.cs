e7308bd5572cff9acb18cee56e811d6b4dc3d9d2
Ratul Bhattacharjee <77481058+Ratul41Bhatt@users.noreply.github.com>
2022-09-24 22:42:31 +0600

    Uploaded

---
+++ CV-v3/Controllers/CVController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV_v3.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Ratul()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult Reference()
+        {
+            return View();
+        }
+
+    }
+}
